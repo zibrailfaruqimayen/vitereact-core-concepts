5cdeac4f6683752e671a8566b6dfa356a8e4ac48
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-03-24 02:07:16 +0600

    vite react

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Device from "./components/Device/Device";
+import Watch from "./components/Watch/Watch";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <Device name="uphone" price="13000"></Device>
       <Device name="Samsung" price="11000"></Device>
+      <Watch></Watch>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
--- src/components/Watch/Watch.jsx
+++ src/components/Watch/Watch.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Watch = () => {
+  const [steps, setSteps] = useState(0);
+
+  const increaseSteps = () => {
+    const newSteps = steps + 1;
+    setSteps(newSteps);
+  };
+
+  useEffect(() => {
+    console.log(steps);
+  }, [steps]);
+
   return (
     <div>
       <h2>This is my smart Watch</h2>
+      <p>Steps: {steps}</p>
+      <button onClick={increaseSteps}>De Dour........</button>
     </div>
   );
 };
